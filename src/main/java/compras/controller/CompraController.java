0b53f09f74a12597dce376bd9ddf05aa03513fad
boris <boris96bs7@gmail.com>
2021-02-12 03:11:50 -0500

    valido completo

--- src/main/java/compras/controller/CompraController.java
+++ src/main/java/compras/controller/CompraController.java
@@ -1,9 +1,10 @@
 package compras.controller;
 
 import java.util.List;
-import java.util.Optional;
+///import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import compras.service.PedidoService;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 public class CompraController {
 
 	@Autowired
--- src/main/java/compras/controller/GastoController.java
+++ src/main/java/compras/controller/GastoController.java
@@ -3,6 +3,7 @@ package compras.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import compras.modelos.Gasto;
 import compras.service.GastoService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class GastoController {
 	
 	@Autowired 
